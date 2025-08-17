bcb6302591ed539dd53f0c8760682dc2d9f4706a
Tomek515 <dvixespada@gmail.com>
2025-08-17 15:40:59 +0200

    hall include

---
+++ include/hall_sensor.cpp
@@ -0,0 +1,33 @@
+#pragma
+
+#include <chrono>
+
+#define VELOCITY_MEASURE_PIN_1 16
+#define VELOCITY_MEASURE_PIN_2 4
+
+const double WHEEL_CIRCUMFERENCE = 1.4444;
+
+class HallSensor {
+public:
+    HallSensor();
+    void begin();
+    void loop();
+
+    float current_velocity;
+
+
+private:
+
+    static void addRotationSensor();
+    void calculateVelocity();
+
+static volatile long rotation_sensor1;
+static volatile long rotation_sensor2;
+
+static std::chrono::steady_clock::time_point interrupt_time;
+
+const std::chrono::milliseconds measure_time_ms{1500};
+
+
+
+};
