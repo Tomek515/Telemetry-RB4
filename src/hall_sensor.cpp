a9d945feb7ccfd6e2e9726a833fa4b9744b3d2da
Tomek515 <dvixespada@gmail.com>
2025-08-20 21:02:24 +0200

    hall

--- src/hall_sensor.cpp
+++ src/hall_sensor.cpp
@@ -3,3 +3,14 @@
 #include <thread>
 #include <chrono>
 #include <atomic>
+
+HallSensor hall_sesnor_instance;
+
+volatile long HallSensor::rotation_sensor1 = 0;
+volatile long HallSensor::rotation_sensor2 = 0;
+
+std::chrono::steady_clock::time_point HallSensor::interrupt_time;
+
+HallSensor::HallSensor() : current_velocity(0.0f) {
+    last_velocity_measure = std::chrono::steady_clock::now();
+}
