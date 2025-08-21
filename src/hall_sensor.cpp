0713e7a89abf3bcab3df00cc180a34b004e47864
Tomek515 <dvixespada@gmail.com>
2025-08-21 21:55:17 +0200

    hall

--- src/hall_sensor.cpp
+++ src/hall_sensor.cpp
@@ -3,7 +3,7 @@
 #include <thread>
 #include <chrono>
 #include <atomic>
-
+#include <iostream>
 HallSensor hall_sesnor_instance;
 
 volatile long HallSensor::rotation_sensor1 = 0;
@@ -13,4 +13,29 @@ std::chrono::steady_clock::time_point HallSensor::interrupt_time;
 
 HallSensor::HallSensor() : current_velocity(0.0f) {
     last_velocity_measure = std::chrono::steady_clock::now();
+}
+
+void HallSensor::begin(){
+    last_velocity_measure = std::chrono::steady_clock::now();
+
+    gpiod_chip* chip = gpiod_chip_open_by_name("gpiochip0");
+    if (!chip) {
+        std::cerr << "Failed to open GPIO chip";
+    };
+
+    gpiod_line* line = gpiod_chip_get_line(chip, VELOCITY_MEASURE_PIN_2);
+if (!line || gpiod_line_request_rising_edge_events(line, "hall_sensor") < 0) {
+        std::cerr << "Failed to request rising edge events for GPIO line\n";
+        return;
+    }
+
+    std::thread([line]() {
+        struct gpiod_line_event event;
+        while (true) {
+            int ret = gpiod_line_event_wait(line, nullptr);
+            if (ret > 0 && gpiod_line_event_read(line, &event) == 0) {
+                HallSensor::addRotationSensor();
+            }
+        }
+    }).detach();
 }
