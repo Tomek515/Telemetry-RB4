9190f9acec55e9435e8d9b277eb2c6a166a9f183
Tomek515 <dvixespada@gmail.com>
2025-08-22 19:24:38 +0200

    Update gpiod_platform.hpp

--- include/gpiod_platform.hpp
+++ include/gpiod_platform.hpp
@@ -15,21 +15,19 @@ struct gpiod_line_request_config {
   #define GPIOD_LINE_REQUEST_DIRECTION_OUTPUT 1
   #endif
 
-  inline gpiod_chip* gpiod_chip_open(const char*)                              { return nullptr; }
-  inline gpiod_chip* gpiod_chip_open_by_name(const char*)                      { return nullptr; }
-  inline gpiod_chip* gpiod_chip_open_by_number(int)                            { return nullptr; }
-  inline void        gpiod_chip_close(gpiod_chip*)                             {}
+  inline gpiod_chip* gpiod_chip_open(const char*)                          { return nullptr; }
+  inline gpiod_chip* gpiod_chip_open_by_name(const char*)                  { return nullptr; }
+  inline gpiod_chip* gpiod_chip_open_by_number(int)                        { return nullptr; }
+  inline void        gpiod_chip_close(gpiod_chip*)                         {}
 
-  inline gpiod_line* gpiod_chip_get_line(gpiod_chip*, int)                     { return nullptr; }
-  inline int         gpiod_line_request_input(gpiod_line*, const char*)        { return 0; }
-  inline int         gpiod_line_request_output(gpiod_line*, const char*, int)  { return 0; }
-  inline int         gpiod_line_request_rising_edge_events(gpiod_line*, const char*)   { return 0; }
-  inline int         gpiod_line_request_falling_edge_events(gpiod_line*, const char*)  { return 0; }
-  inline int         gpiod_line_request_both_edges_events(gpiod_line*, const char*)    { return 0; }
+  inline gpiod_line* gpiod_chip_get_line(gpiod_chip*, int)                 { return nullptr; }
+  inline int         gpiod_line_request_rising_edge_events(gpiod_line*, const char*) { return 0; }
+  inline int         gpiod_line_request_falling_edge_events(gpiod_line*, const char*){ return 0; }
+  inline int         gpiod_line_request_both_edges_events(gpiod_line*, const char*)  { return 0; }
+  inline void        gpiod_line_release(gpiod_line*)                       {}
+  inline int         gpiod_line_set_value(gpiod_line*, int)                { return 0; }
 
-  inline int         gpiod_line_event_wait(gpiod_line*, const struct timespec*) { return 0; } // brak zdarzeń
-  inline int         gpiod_line_event_read(gpiod_line*, struct gpiod_line_event*) { return -1; }
-
-  inline int         gpiod_line_set_value(gpiod_line*, int)                    { return 0; }
+  inline int         gpiod_line_event_wait(gpiod_line*, const struct timespec*) { return 0; }   // 0 = brak zdarzeń
+  inline int         gpiod_line_event_read(gpiod_line*, struct gpiod_line_event*) { return -1; } // brak danych
   inline void        gpiod_line_release(gpiod_line*)                           {}
 #endif
